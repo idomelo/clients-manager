9bde8a7c33e5f8b7ade6c5c9ba54b2ab15e80ca9
Ido Melo <idomelo123@gmail.com>
2022-04-27 17:05:55 -0300

    error handling + snackbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -23,8 +24,6 @@ import { FormComponent } from './components/form/form.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-
-
     MatToolbarModule,
     MatInputModule,
     MatSelectModule,
@@ -32,6 +31,7 @@ import { FormComponent } from './components/form/form.component';
     MatTableModule,
     HttpClientModule,
     MatProgressSpinnerModule,
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/clients-list/clients-list.component.ts
+++ src/app/components/clients-list/clients-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { Client } from 'src/app/model/client';
 import { ClientsService } from 'src/app/services/clients.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
 @Component({
   selector: 'app-clients-list',
   templateUrl: './clients-list.component.html',
@@ -11,11 +12,28 @@ export class ClientsListComponent implements OnInit {
 
   clients$: Observable<Client[]>;
   displayedColumns: string[] = ['id', 'name', 'priority'];
-  // clientsService: ClientsService;
 
-  constructor(private clientsService: ClientsService) {
-    // this.clientsService = new ClientsService();
-    this.clients$ = this.clientsService.list();
+  messageError: string = 'Erro ao carregar a Lista, tente novamente mais tarde';
+  actionError: string = 'OK';
+  durationMessage: number = 3000;
+
+  constructor(
+    private clientsService: ClientsService, 
+    private _snackBar: MatSnackBar
+    ) {
+    this.clients$ = this.clientsService.list()
+    .pipe(
+      catchError(error => {
+        this.onError();
+        return of([])
+      })
+    );
+  }
+
+  onError() {
+    this._snackBar.open(this.messageError, this.actionError, {
+      duration: this.durationMessage,
+    });
   }
 
   ngOnInit(): void {
